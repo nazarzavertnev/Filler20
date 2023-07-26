313088934ee02cf0a209b64a77af57b49ae858ad
nazarzavertnev <nazar.zavertnev@yandex.ru>
2023-07-26 14:43:23 +0300

    Design change + init 1 function

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-from PyQt5 import uic
+from PyQt5 import uic, QtGui, QtTest
 from PyQt5.QtWidgets import QApplication
 
 import winsound
 
-import time
-
-import threading
-
-from PyQt5 import QtTest
+import json
 
 Form, Window = uic.loadUiType("test.ui")
 Form2, Window2 = uic.loadUiType("test2.ui")
@@ -16,13 +12,13 @@ app = QApplication([])
 window = Window()
 form = Form()
 form.setupUi(window)
+window.setFixedSize(447, 623)
+
 
 window2 = Window2()
 form2 = Form2()
 form2.setupUi(window2)
 winsound.PlaySound("sound\startap.wav", winsound.SND_ASYNC)
-
-event = threading.Event()
 
 window2.show()
 while(form2.progressBar.value()!=100):
@@ -33,14 +29,30 @@ window2.close()
 QtTest.QTest.qWait(1500)
 window.show()
 
-winsound.PlaySound("sound\startup.wav", winsound.SND_ASYNC)
+winsound.PlaySound("sound\cient.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
 
-def test():
-    winsound.PlaySound("sound\qwerty4.wav", winsound.SND_ASYNC)
-def test2():
-    winsound.PlaySound("sound\qwerty5.wav", winsound.SND_ASYNC)
+for i in range(6):
+    for j in range(99):
+        form.dial.setValue(j)
+        QtTest.QTest.qWait(10)
 
-form.lineEdit.textChanged.connect(test)
-form.lineEdit.returnPressed.connect(test2)
+QtTest.QTest.qWait(3480)
+
+codes_file = open('eschf\codes.json', encoding="utf8")
+codes_text = codes_file.read()
+codes_file.close()
+codes = json.loads(codes_text)
+print(codes)
+
+form.dial.hide()
+
+form.label_3.setText("Добро пожаловать!")
+form.eschf.setEnabled(True)
+form.eschf_status.setText("Всё хорошо")
+
+def eschf_enter():
+    winsound.PlaySound("sound\enter.wav", winsound.SND_ASYNC)
+
+form.eschf.clicked.connect(eschf_enter)
 
 app.exec()
